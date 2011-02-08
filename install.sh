50583fa1e49d9e7f687c9f7f6555c9629329e95d
Christian Eichhorn <ceichhor@laptop.(none)>
2011-02-08 02:03:03 +0100

    install.sh fixed

--- install.sh
+++ install.sh
@@ -5,8 +5,7 @@ git submodule update
 ln -sf $(pwd)/config/terminator/config ~/.config/terminator/config
 ln -sf $(pwd)/gvimrc ~/.gvimrc
 ln -sf $(pwd)/vimrc ~/.vimrc
-ln -sf $(pwd)/zshrc ~/.zsh
 ln -sf $(pwd)/zshrc ~/.zshrc
 
 ln -sf $(pwd)/vim ~/.vim
-ln -sf $(pwd)/vim ~/.zsh
+ln -sf $(pwd)/zsh ~/.zsh
